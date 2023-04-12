376b0f1c6c03cba12254050d7b7dab3e978fc933
djuraev <zdorovennovalex@gmail.com>
2023-04-12 21:45:57 +0300

    Initial commit

---
+++ src/main/kotlin/ru/test/Application.kt
@@ -0,0 +1,15 @@
+package ru.test
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import ru.test.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
